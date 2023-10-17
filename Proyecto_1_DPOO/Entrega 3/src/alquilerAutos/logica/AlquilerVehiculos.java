e8b6b862807f10b3b0485b351c18d3fbb686a6ff
Cristian13c <123591685+Cristian13c@users.noreply.github.com>
2023-10-16 22:21:29 -0500

    consola admin local y calcular precio

--- Proyecto_1_DPOO/Entrega 3/src/alquilerAutos/logica/AlquilerVehiculos.java
+++ Proyecto_1_DPOO/Entrega 3/src/alquilerAutos/logica/AlquilerVehiculos.java
@@ -1,5 +1,7 @@
 package alquilerAutos.logica;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -544,7 +546,25 @@ public class AlquilerVehiculos
 			String fechaHoraInicio, String fechaHoraFinal)
 	{
 		
-		return numeroConductores;
+		Tarifa tarifa = tarifas.get(categoria);
+		
+		int cambioSede = 0;
+		
+		LocalDate fechaInicial = LocalDate.parse(fechaHoraInicio);
+        LocalDate fechaFinal = LocalDate.parse(fechaHoraFinal);
+        Period periodo = Period.between(fechaInicial, fechaFinal);
+        
+        int tarifaDiaria = tarifa.getTarifaDiaria();
+        int dias = periodo.getDays();
+        
+        if(!sedeOrigen.equals(sedeEntrega)) 
+        {
+        	cambioSede = 30000;
+        }
+        int precio = (dias*tarifaDiaria)+(numeroConductores*20000)+cambioSede;
+		
+		
+		return precio;
 
 	}
 	
@@ -565,6 +585,8 @@ public class AlquilerVehiculos
 		
 		return retorno;
 	}
+	
+	
 	
 	
 	/**
@@ -598,4 +620,5 @@ public class AlquilerVehiculos
 		
 		return retorno;
 	}
+	
 }
